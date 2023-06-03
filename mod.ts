a88566b0af8a123d1108ad7f0fbfa21fb032d9ca
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-06-04 01:14:12 +0900

    feat(mod): export format and related types

---
+++ mod.ts
@@ -0,0 +1,9 @@
+// Copyright 2023-latest Tomoki Miyauchi. All rights reserved. MIT license.
+// This module is browser compatible.
+
+export {
+  type ExtractSpecifier,
+  format,
+  type FormatOptions,
+  type Placeholder,
+} from "./format.ts";
