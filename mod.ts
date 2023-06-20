69cd2157c676196a89389aaa95586d383e56ff46
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-06-20 10:16:26 +0900

    docs(mod): add module description

--- mod.ts
+++ mod.ts
@@ -1,6 +1,10 @@
 // Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
 // This module is browser compatible.
 
+/** Formatting and printing string utilities.
+ * @module
+ */
+
 export {
   type ExtractSpecifier,
   format,
